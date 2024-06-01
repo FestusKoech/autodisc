95ecf0a68d2796ffd1f99d31c4489df169dfcccf
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-06-01 17:47:56 +0300

    index files

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,9 +1,16 @@
 function LeftSidebar (){
 
-    return  
-    <h1>
-        Left
-    </h1>
+    return  (
+
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                
+
+            </div>
+
+        </section>
+    )
+ 
 
 
 }
