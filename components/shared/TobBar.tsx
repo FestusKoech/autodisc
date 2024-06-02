82bb9a75daa098b9e162005d4ed2d19e46523003
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-06-02 12:19:37 +0300

    isActive implemented

--- components/shared/TobBar.tsx
+++ components/shared/TobBar.tsx
@@ -8,7 +8,7 @@ function TopBar (){
     <nav className="topbar">
         
        <Link href='/' className='flex items-center gap-4'>
-        <Image src='logo.svg' alt='logo' width={28} height={28}/>
+        <Image src='/logo.svg' alt='logo' width={28} height={28}/>
 
         <p className="text-heading3-bold text-light-1 max-xs:hidden">
             Webscale
@@ -25,7 +25,7 @@ function TopBar (){
             <SignOutButton>
                 <div className="flex cursor-pointer">
 
-                    <Image src='logout.svg' alt='logout' width={24} height={24} />
+                    <Image src='/assets/logout.svg' alt='logout' width={24} height={24} />
 
                 </div>
             </SignOutButton>
