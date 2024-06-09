138408c5445de17d768c3f75cca5181213b9ccfa
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2024-06-09 18:35:49 +0300

    added forms

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,9 +1,17 @@
-import * as z from 'zod';
+import * as z from "zod";
 
-export const userValidation = z.object({
-    profile_photo: z.string().url().nonempty(),
-    name: z.string().min(3,{message: 'minimum of 3 characters'}).max(30),
-    username: z.string().min(3, {message: 'minimum of 3 characters'}).max(10, {message:'maximum of 10 characters'}),
-    bio: z.string().min(3, {message:'mimimum of 3 characters'}).max(1000, {message:'maximum of 1000 characters'}),
-
-})
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),
+  name: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  username: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  bio: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(1000, { message: "Maximum 1000 caracters." }),
+});
